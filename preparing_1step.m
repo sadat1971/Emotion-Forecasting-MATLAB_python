1d4326b787464d3229affc0969b05bc720de7da6
sadat1971 <44307453+sadat1971@users.noreply.github.com>
2018-10-22 18:16:56 -0400

    Add files via upload

--- preparing_1step.m
+++ preparing_1step.m
@@ -1,19 +1,28 @@
-%This code prepare data for the task of 1-step forecasting
-%find the average lengths of values
-LENS=[];
-MEANS=[];
-STDS=[];
+%% This code prepare data for the task of 1-step forecasting
+%At the end of the code, we will find a prepared dataset for 1- step
+%forecasting. The preparation includes normalization and zero padding at
+%the end 
+
+
+
+LENS=[]; %It will save the lengths of features. The longest utterance will have the biggest value
+MEANS=[];  %for saving the means for normalizartion
+STDS=[]; %for saving the stds for normalization
 NN=[];
 st=1; %change it to 2,3 for step 2 and 3
 %Let's shift the emotion label by 1 step
 for sp=1:10
     gathered_data=[];
     AVd=[];
-    All_data=[];
+    All_data=[]; %will save the n-step shifted data and labels
     gathered_data=load(sprintf('Sadat/IEMOCAP_forcasting/sameframe/All_audiovisual/AV_samelength_s_%d.mat',sp));
     j=1;
-    for  i=1:(length(gathered_data.AV)-st)
+    for  i=1:(length(gathered_data.AV)-st) %remember, the last utterance will not have any labels.
         
+        
+        %First, let's compare the current utterance and to-be-forecasted
+        %utterance. If they are of same speaker and same dialog, record
+        %that. Otherwise, ignore.
         next_name=gathered_data.AV(i+st).name;
         
         a=split(next_name,'_F');
@@ -28,11 +37,11 @@ for sp=1:10
         if(strcmp(name1{1},name2{1}))
             All_data(j).label=gathered_data.AV(i+st).label;
         else
-            All_data(j).label=10;
+            All_data(j).label=10; % There is no label named '10'. We are using that just to keep track of the utterances that need to be removed.
         end
         
         
-        if All_data(j).label<4
+        if All_data(j).label<4  %we will take 0-3 labels only
             LENS=[LENS;size(All_data(j).audio,1)];
             AVd=[AVd;All_data(j).audio All_data(j).video];
         end
@@ -45,15 +54,15 @@ for sp=1:10
     save(sprintf('Sadat/IEMOCAP_forcasting/sameframe/%d_step/emo_shifted/step%d_emoshifted_s_%d.mat',st,st,sp),'All_data');
 end
 
-    
-    
+
+
 
 M=floor(max(LENS));
 
 
-%Normalize the data
+%% Normalize the data
 for sp=1:10
-    normalized_semi=[];
+    normalized_semi=[]; %That will contain the normalized data
     DATA=[];
     DATA=load(sprintf('Sadat/IEMOCAP_forcasting/sameframe/%d_step/emo_shifted/step%d_emoshifted_s_%d.mat',st,st,sp));
     k=1;
@@ -65,7 +74,9 @@ for sp=1:10
             Full_data=[DATA.All_data(i).audio DATA.All_data(i).video];
             normal=[];
             normal=(Full_data-MEANS(sp,:))./STDS(sp,:);
-            normalized_semi(k).data=padarray(normal,M-size(normal,1),0,'post');
+            normalized_semi(k).data=padarray(normal,M-size(normal,1),0,'post'); %this is zero-padding at the end. If the
+            %utternace is smaller than the longer utterance, pad 0 at the end and make
+            %it of same size
             normalized_semi(k).label=DATA.All_data(i).label;
             k=k+1;
         end
